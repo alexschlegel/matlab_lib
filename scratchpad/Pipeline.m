64ce657bc14e983c4262de8ddca745855a1f7235
Bennet Vance <bennet@cs.dartmouth.edu>
2015-01-22 14:30:12 -0500

    Changed class to support top-level invocation with or without debug
    Top-level static methods now include both of the following:
      Pipeline.debugTrainClassify
      Pipeline.runTrainClassify

--- scratchpad/Pipeline.m
+++ scratchpad/Pipeline.m
@@ -16,13 +16,16 @@ classdef Pipeline
 %connectivity from X to Y?
 
 
-% Simplest invocation (as of 2015-01-22):  Pipeline.runTrainClassify;
+% Simplest invocation (as of 2015-01-22):  Pipeline.debugTrainClassify;
+% use "Pipeline.runTrainClassify" to randomize behavior and suppress
+% diagnostic output.
 %
 
 properties
 	uopt
 end
 properties (SetAccess = private)
+	explicitOptionNames
 end
 
 methods
@@ -32,8 +35,10 @@ methods
 	%
 	% In:
 	%	<options>:
-	%					[In future, default for DEBUG may change to false]
-	%		DEBUG		(true) Display debugging information
+	%					-- Debugging options:
+	%
+	%		DEBUG		(false) Display debugging information
+	%		seed:		(randseed2) the base seed to use for randomizing
 	%
 	%					-- Size of the various spaces:
 	%
@@ -64,23 +69,36 @@ methods
 	function obj = Pipeline(varargin)
 		%user-defined parameters (with defaults)
 		opt	= ParseArgs(varargin,...
-			'DEBUG'		, true	, ...
-			'nSig'		, 10	, ...
-			'nSigCause'	, 10	, ...
-			'nVoxel'	, 100	, ...
-			'nTBlock'	, 20	, ...
-			'nTRest'	, 5		, ...
-			'nRepBlock'	, 3		, ...
-			'nRun'		, 10	, ...
-			'CRecurX'	, 0.1	, ...
-			'CRecurY'	, 0.7	, ...
-			'CRecurZ'	, 0		, ...
-			'WFullness'	, 0.1	, ...
-			'WSum'		, 0.1	, ...
-			'doMixing'	, true	, ...
+			'DEBUG'		, false		, ...
+			'seed'		, randseed2	, ...
+			'nSig'		, 10		, ...
+			'nSigCause'	, 10		, ...
+			'nVoxel'	, 100		, ...
+			'nTBlock'	, 20		, ...
+			'nTRest'	, 5			, ...
+			'nRepBlock'	, 3			, ...
+			'nRun'		, 10		, ...
+			'CRecurX'	, 0.1		, ...
+			'CRecurY'	, 0.7		, ...
+			'CRecurZ'	, 0			, ...
+			'WFullness'	, 0.1		, ...
+			'WSum'		, 0.1		, ...
+			'doMixing'	, true		, ...
 			'noiseMix'	, 0.1	  ...
 			);
-		obj.uopt = opt;
+		obj.uopt				= opt;
+		obj.explicitOptionNames	= varargin(1:2:end);
+	end
+
+	function obj = changeDefaultsToDebug(obj)
+		obj = obj.changeOptionDefault('DEBUG',true);
+		obj = obj.changeOptionDefault('seed',0);
+	end
+
+	function obj = changeOptionDefault(obj,optionName,newDefault)
+		if ~ismember(optionName,obj.explicitOptionNames)
+			obj.uopt.(optionName)	= newDefault;
+		end
 	end
 
 	function [acc,p_binom] = trainClassify(obj)
@@ -90,14 +108,13 @@ methods
 %TODO: Fix indentation throughout
 
 
-%for debugging, make behavior reproducible, otherwise randomize
-	seeds			= [0 randseed2];
-	rng(seeds([DEBUG ~DEBUG]),'twister');
+%initialize pseudo-random-number generator
+	rng(u.seed,'twister');
 
 %the two causality matrices (and other control causality matrices)
 	nW				= 4;
 	[cWCause,cW]	= deal(cell(nW,1));
-	
+
 	for kW=1:nW
 		%generate a random W
 			W					= rand(u.nSigCause);
@@ -106,30 +123,30 @@ methods
 		%normalize each column to the specified mean
 			W			= W*u.WSum./repmat(sum(W,1),[u.nSigCause 1]);
 			W(isnan(W))	= 0;
-		
+
 		cWCause{kW}	= W;
-		
+
 		%insert into the full matrix
 		cW{kW}							= zeros(u.nSig);
 		cW{kW}(1:u.nSigCause,1:u.nSigCause)	= cWCause{kW};
 	end
-	
+
 	[WACause,WBCause,WBlankCause,WZCause]	= deal(cWCause{:});
 	[WA,WB,WBlank,WZ]						= deal(cW{:});
-	
+
 	if DEBUG
 		szIm	= 200;
-		
+
 		im	= normalize([WACause WBCause]);
 		im	= [imresize(im(:,1:u.nSigCause),[szIm NaN],'nearest') 0.8*ones(szIm,round(200/u.nSigCause)) imresize(im(:,u.nSigCause+1:end),[szIm NaN],'nearest')];
 		figure; imshow(im);
 		title('W_A and W_B');
-		
+
 		im	= normalize([WBlankCause WZCause]);
 		im	= [imresize(im(:,1:u.nSigCause),[szIm NaN],'nearest') 0.8*ones(szIm,round(200/u.nSigCause)) imresize(im(:,u.nSigCause+1:end),[szIm NaN],'nearest')];
 		figure; imshow(im);
 		title('W_{blank} and W_Z');
-		
+
 		disp(sprintf('WA column sums:  %s',sprintf('%.3f ',sum(WACause))));
 		disp(sprintf('WB column sums:  %s',sprintf('%.3f ',sum(WBCause))));
 		disp(sprintf('sum(WA)+CRecurY: %s',sprintf('%.3f ',sum(WACause)+u.CRecurY)));
@@ -142,12 +159,12 @@ methods
 		block	= blockdesign(1:2,u.nRepBlock,u.nRun,'seed',rngState.Seed+1);
 		rng(rngState);
 		target	= arrayfun(@(run) block2target(block(run,:),u.nTBlock,u.nTRest,{'A','B'}),reshape(1:u.nRun,[],1),'uni',false);
-	
+
 	%number of time points per run
 		nTRun	= numel(target{1});
 	%total number of time points
 		nT		= nTRun*u.nRun;
-	
+
 	if DEBUG
 		disp(sprintf('TRs per run: %d',nTRun));
 
@@ -162,7 +179,7 @@ methods
 %generate the functional signals
 	[X,Y]	= deal(zeros(nTRun,u.nRun,u.nSig));
 	Z		= zeros(nTRun,u.nRun,u.nSig,u.nSig);
-	
+
 	for kR=1:u.nRun
 		for kT=1:nTRun
 			%previous values
@@ -184,7 +201,7 @@ methods
 					otherwise
 						W	= WBlank;
 				end
-			
+
 			%pre-source
 				Z(kT,kR,:,:)	= u.CRecurZ.*zPrev + (1-u.CRecurZ).*randn(u.nSig,u.nSig);
 			%source
@@ -193,57 +210,57 @@ methods
 				Y(kT,kR,:)		= u.CRecurY.*yPrev + W'*xPrev + (1-u.WSum/sqrt(u.nSigCause)-u.CRecurY).*randn(u.nSig,1);
 		end
 	end
-	
+
 	if DEBUG
 		XCause	= X(:,:,1:u.nSigCause);
 		YCause	= Y(:,:,1:u.nSigCause);
-		
+
 		cMeasure	= {'mean','range','std','std(d/dx)'};
 		cFMeasure	= {@mean,@range,@std,@(x) std(diff(x))};
-		
+
 		cXMeasure	= cellfun(@(f) f(reshape(permute(XCause,[1 3 2]),nTRun*u.nSigCause,u.nRun)),cFMeasure,'uni',false);
 		cYMeasure	= cellfun(@(f) f(reshape(permute(YCause,[1 3 2]),nTRun*u.nSigCause,u.nRun)),cFMeasure,'uni',false);
-		
+
 		cXMMeasure	= cellfun(@mean,cXMeasure,'uni',false);
 		cYMMeasure	= cellfun(@mean,cYMeasure,'uni',false);
-		
+
 		[h,p,ci,stats]	= cellfun(@ttest2,cXMeasure,cYMeasure,'uni',false);
 		tstat			= cellfun(@(s) s.tstat,stats,'uni',false);
-		
+
 		disp(sprintf('XCause mean/range/std/std(d/dx): %.3f %.3f %.3f %.3f',cXMMeasure{:}));
 		disp(sprintf('YCause mean/range/std/std(d/dx): %.3f %.3f %.3f %.3f',cYMMeasure{:}));
 		disp(sprintf('p      mean/range/std/std(d/dx): %.3f %.3f %.3f %.3f',p{:}));
 		disp(sprintf('tstat  mean/range/std/std(d/dx): %.3f %.3f %.3f %.3f',tstat{:}));
 	end
-	
+
 	if DEBUG
 		tPlot	= reshape(1:nTRun,[],1);
 		xPlot	= X(:,1,1);
 		yPlot	= Y(:,1,1);
-		
+
 		h		= alexplot(tPlot,{xPlot yPlot},...
 					'title'		, 'Run 1'		, ...
 					'xlabel'	, 't'			, ...
 					'ylabel'	, 'Amplitude'	, ...
 					'legend'	, {'X','Y'}		  ...
 					);
-		
+
 		yLim	= get(h.hA,'ylim');
 		yMin	= yLim(1);
 		yMax	= yLim(2);
-		
+
 		col	= {[0.25 0.25 0.25]; [0.75 0.75 0.75]};
-		
+
 		for kB=1:size(block,2)
 			blockCur	= block(1,kB);
 			colCur		= col{blockCur};
-			
+
 			kStart		= u.nTRest + 1 + (kB-1)*(u.nTBlock+u.nTRest);
 			kEnd		= kStart + u.nTBlock;
-			
+
 			tStart	= tPlot(kStart);
 			tEnd	= tPlot(kEnd);
-			
+
 			hP	= patch([tStart;tStart;tEnd;tEnd],[yMin;yMax;yMax;yMin],colCur);
 			set(hP,'EdgeColor',colCur);
 			MoveToBack(h.hA,hP);
@@ -260,13 +277,13 @@ methods
 	if u.doMixing
 		[CPCAX,XUnMix]	= pca(reshape(XMix,nT,u.nVoxel));
 		XUnMix			= reshape(XUnMix,nTRun,u.nRun,u.nVoxel);
-		
+
 		[CPCAY,YUnMix]	= pca(reshape(YMix,nT,u.nVoxel));
 		YUnMix			= reshape(YUnMix,nTRun,u.nRun,u.nVoxel);
 	else
 		[XUnMix,YUnMix]	= deal(X,Y);
 	end
-	
+
 	XUnMix	= XUnMix(:,:,1:u.nSigCause);
 	YUnMix	= YUnMix(:,:,1:u.nSigCause);
 
@@ -274,13 +291,13 @@ methods
 	%get the A and B portions of the signals for each run
 		XA	= arrayfun(@(run) squeeze(XUnMix(strcmp(target{run},'A'),run,:)),reshape(1:u.nRun,[],1),'uni',false);
 		XB	= arrayfun(@(run) squeeze(XUnMix(strcmp(target{run},'B'),run,:)),reshape(1:u.nRun,[],1),'uni',false);
-		
+
 		YA	= arrayfun(@(run) squeeze(YUnMix(strcmp(target{run},'A'),run,:)),reshape(1:u.nRun,[],1),'uni',false);
 		YB	= arrayfun(@(run) squeeze(YUnMix(strcmp(target{run},'B'),run,:)),reshape(1:u.nRun,[],1),'uni',false);
-	
+
 	%calculate the Granger Causality from X components to Y components for each run
 		[WAs,WBs]	= deal(repmat({zeros(u.nSigCause)},[u.nRun 1]));
-		
+
 		for kR=1:u.nRun
 			for kX=1:u.nSigCause
 				for kY=1:u.nSigCause
@@ -289,47 +306,47 @@ methods
 				end
 			end
 		end
-	
+
 	if DEBUG
 		mWAs	= mean(cat(3,WAs{:}),3);
 		mWBs	= mean(cat(3,WBs{:}),3);
-		
+
 		im	= normalize([mWAs mWBs]);
 		im	= [imresize(im(:,1:u.nSigCause),[szIm NaN],'nearest') 0.8*ones(szIm,round(200/u.nSigCause)) imresize(im(:,u.nSigCause+1:end),[szIm NaN],'nearest')];
 		figure; imshow(im);
 		title('W^*_A and W^*_B');
-		
+
 		disp(sprintf('mean W*A column sums:  %s',sprintf('%.3f ',sum(mWAs))));
 		disp(sprintf('mean W*B column sums:  %s',sprintf('%.3f ',sum(mWBs))));
 	end
 
 %classify between W*A and W*B
 	P	= cvpartition(u.nRun,'LeaveOut');
-	
+
 	res	= zeros(P.NumTestSets,1);
 	for kP=1:P.NumTestSets
 		WATrain	= cellfun(@(W) reshape(W,1,[]), WAs(P.training(kP)),'uni',false);
 		WBTrain	= cellfun(@(W) reshape(W,1,[]), WBs(P.training(kP)),'uni',false);
-		
+
 		WATest	= cellfun(@(W) reshape(W,1,[]), WAs(P.test(kP)),'uni',false);
 		WBTest	= cellfun(@(W) reshape(W,1,[]), WBs(P.test(kP)),'uni',false);
-		
+
 		WATrain	= cat(1,WATrain{:});
 		WBTrain	= cat(1,WBTrain{:});
 		WATest	= cat(1,WATest{:});
 		WBTest	= cat(1,WBTest{:});
-		
+
 		WTrain	= [WATrain; WBTrain];
 		WTest	= [WATest; WBTest];
-		
+
 		lblTrain	= reshape(repmat({'A' 'B'},[u.nRun-1 1]),[],1);
 		lblTest		= {'A';'B'};
-		
+
 		sSVM	= svmtrain(WTrain,lblTrain);
 		pred	= svmclassify(sSVM,WTest);
 		res(kP)	= sum(strcmp(pred,lblTest));
 	end
-	
+
 	%one-tailed binomial test
 		Nbin	= 2*u.nRun;
 		Pbin	= 0.5;
@@ -337,15 +354,42 @@ methods
 		p_binom	= 1 - binocdf(Xbin-1,Nbin,Pbin);
 	%accuracy
 		acc	= Xbin/Nbin;
-		
+
 	if DEBUG
 		disp(sprintf('accuracy: %.2f%%',100*acc));
 		disp(sprintf('p(binom): %.3f',p_binom));
 	end
-	
+
 	end
 end
 methods (Static)
+	% createDebugPipeline - static method for creating debug-pipeline
+	%
+	% Syntax:	pipeline = Pipeline.createDebugPipeline(<options>)
+	%
+	% In:
+	%	<options>:
+	%		See Pipeline constructor above for description of <options>,
+	%		but note that this method overrides the default debugging options
+	%
+	function pipeline = createDebugPipeline(varargin)
+		pipeline = Pipeline(varargin{:});
+		pipeline = pipeline.changeDefaultsToDebug;
+	end
+
+	% debugTrainClassify - static method for running debug-pipeline
+	%
+	% Syntax:	[acc,p_binom] = Pipeline.debugTrainClassify(<options>)
+	%
+	% In:
+	%	<options>:
+	%		See createDebugPipeline above for description of <options>
+	%
+	function [acc,p_binom] = debugTrainClassify(varargin)
+		pipeline = Pipeline.createDebugPipeline(varargin{:});
+		[acc,p_binom] = pipeline.trainClassify;
+	end
+
 	% runTrainClassify - static method for running pipeline
 	%
 	% Syntax:	[acc,p_binom] = Pipeline.runTrainClassify(<options>)

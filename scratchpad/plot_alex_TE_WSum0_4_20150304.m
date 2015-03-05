646e862f4626d154e79092e5b3ba30730373848d
Bennet Vance <bennet@cs.dartmouth.edu>
2015-03-05 17:05:11 -0500

    Added plot-capsules and figures for run with WSum=0.4

---
+++ scratchpad/plot_alex_TE_WSum0_4_20150304.m
@@ -0,0 +1,17 @@
+
+% Script to create figures from exploratory data capsules
+%
+
+load('scratchpad/capsules/20150305_052504_iflow_plot_data.mat');
+pp			= Pipeline;
+cCapsule	= plot_data.capsuleCell;
+
+for kC=1:numel(cCapsule)
+	p		= pp.makePlotFromCapsule(cCapsule{kC});
+	h(kC)	= p.hF;
+end
+
+figfilename	= ['20150304_alex_TE_WSum0.4_plotted_' FormatTime(nowms,'yyyymmdd_HHMM') '.fig'];
+savefig(h,figfilename);
+fprintf('Plots saved to %s\n',figfilename);
+

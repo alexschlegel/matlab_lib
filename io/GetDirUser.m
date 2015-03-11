f994174354d701e8bcd48e2bf349be66dd3754ee
Alex Schlegel <schlegel@gmail.com>
2015-03-11 13:51:41 -0400

    renamed GetHomeDir to GetDirUser added case for GetDirUser where no HOME env variable exists

---
+++ io/GetDirUser.m
@@ -0,0 +1,31 @@
+function strDirUser = GetDirUser()
+% GetDirUser
+% 
+% Description:	get a user's home directory
+% 
+% Syntax:	strDirHome = GetHomeDir()
+% 
+% Updated: 2015-03-11
+% Copyright 2015 Alex Schlegel (schlegel@gmail.com).  This work is licensed
+% under a Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported
+% License.
+if ispc
+	strDirUser	= getenv('USERPROFILE');
+else
+	strDirUser	= getenv('HOME');
+	
+	if isempty(strDirUser)
+		try
+			[ec,strUser]	= system('whoami');
+			strUser			= StringTrim(strUser);
+			if ~isempty(strUser)
+				strDirUser	= DirAppend('/home/',strUser);
+			end
+		catch
+		end
+	end
+end
+
+if ~isdir(strDirUser)
+	strDirUser	= '';
+end

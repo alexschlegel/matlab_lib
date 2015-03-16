85e45e061ce284bcaebd6a8e3a0318e53a18907d
Alex Schlegel <schlegel@gmail.com>
2015-03-16 12:43:07 -0400

    fixed bug in FSLFEATPreprocess when no files were to be processed

--- research/MRI/FSLFEATPreprocess.m
+++ research/MRI/FSLFEATPreprocess.m
@@ -66,7 +66,7 @@ function [bSuccess,cPathOut,tr,cDirFEAT] = FSLFEATPreprocess(cPathData,varargin)
 %	tr			- the TR of each processed data file
 %	cDirFEAT	- the path/cell of paths to preprocessing feat directories
 % 
-% Updated: 2015-03-13
+% Updated: 2015-03-15
 % Copyright 2015 Alex Schlegel (schlegel@gmail.com).  This work is licensed
 % under a Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported
 % License.
@@ -142,15 +142,19 @@ opt.standard	= conditional(bRegStandard,opt.standard,'');
 					opt
 				};
 	
-	[mtO,tr]	= MultiTask(@PreprocessOne,cInput,...
-					'description'	, 'Preprocessing functional data using FEAT'	, ...
-					'catch'			, true											, ...
-					'nthread'		, opt.nthread									, ...
-					'silent'		, opt.silent									  ...
-					);
-	
-	bSuccess(bProcess)	= cellfun(@notfalse,mtO);
-	tr					= cellfun(@(x) unless(x,NaN),tr);
+	if any(bProcess)
+		[mtO,tr]	= MultiTask(@PreprocessOne,cInput,...
+						'description'	, 'Preprocessing functional data using FEAT'	, ...
+						'catch'			, true											, ...
+						'nthread'		, opt.nthread									, ...
+						'silent'		, opt.silent									  ...
+						);
+		
+		bSuccess(bProcess)	= cellfun(@notfalse,mtO);
+		tr					= cellfun(@(x) unless(x,NaN),tr);
+	else
+		tr	= [];
+	end
 %uncellify
 	if bNoCell
 		cPathOut	= cPathOut{1};

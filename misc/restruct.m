d97540350409c3025732082ede53bae3b4af19e7
Alex Schlegel <schlegel@gmail.com>
2015-07-02 09:44:24 -0400

    added block to handle empty cell input

--- misc/restruct.m
+++ misc/restruct.m
@@ -14,7 +14,7 @@ function s = restruct(s,varargin)
 %				struct of arrays. this helps resolve ambiguity in cases of
 %				struct arrays with one element.
 % 
-% Updated: 2015-04-15
+% Updated: 2015-07-02
 % Copyright 2015 Alex Schlegel (schlegel@gmail.com).  This work is licensed
 % under a Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported
 % License.
@@ -44,6 +44,11 @@ function s = restruct(s,varargin)
 
 
 if iscell(s)
+	if isempty(s)
+		s	= struct;
+		return;
+	end
+	
 	[cf,uf]	= cellnestflatten(s);
 	sf		= cell2mat(cf);
 	sr		= restruct(sf,'array',true);
